46a0b1d36e48f92056f2214580ade4a98f96376c
samystudio <contact@samystudio.net>
2018-03-31 00:44:45 +0200

    Refactoring + cleanup

--- app/src/main/kotlin/net/samystudio/beaver/ext/Validator.kt
+++ app/src/main/kotlin/net/samystudio/beaver/ext/Validator.kt
@@ -2,20 +2,20 @@ package net.samystudio.beaver.ext
 
 import android.util.Patterns
 
-fun <T> T.validate(validator: IValidator<T>): Boolean = validator.validate(this)
+fun <T> T.validate(validator: Validator<T>): Boolean = validator.validate(this)
 
-interface IValidator<in T>
+interface Validator<in T>
 {
     fun validate(any: T?): Boolean
 }
 
-class EmailValidator : IValidator<CharSequence>
+class EmailValidator : Validator<CharSequence>
 {
     override fun validate(any: CharSequence?): Boolean =
         !any.isNullOrBlank() && Patterns.EMAIL_ADDRESS.matcher(any).matches()
 }
 
-class PasswordValidator : IValidator<CharSequence>
+class PasswordValidator : Validator<CharSequence>
 {
     override fun validate(any: CharSequence?): Boolean = !any.isNullOrBlank() && any!!.length >= 8
 }
