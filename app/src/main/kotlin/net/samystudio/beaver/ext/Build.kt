7f359e01c6f221ba717d6ccd3e79638d8930f2d7
samystudio <contact@samystudio.net>
2018-11-26 23:42:05 +0100

    Add isEmulator extension

---
+++ app/src/main/kotlin/net/samystudio/beaver/ext/Build.kt
@@ -0,0 +1,12 @@
+package net.samystudio.beaver.ext
+
+import android.os.Build
+
+fun isEmulator() = Build.FINGERPRINT.startsWith("generic")
+        || Build.FINGERPRINT.startsWith("unknown")
+        || Build.MODEL.contains("google_sdk")
+        || Build.MODEL.contains("Emulator")
+        || Build.MODEL.contains("Android SDK built for x86")
+        || Build.MANUFACTURER.contains("Genymotion")
+        || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+        || Build.PRODUCT == "google_sdk"
