3ed2781752bd6abc12d58f9b6d9632369b25af2c
SamYStudiO <contact@samystudio.net>
2019-10-24 01:10:23 +0200

    Fix API url

--- app/src/main/kotlin/net/samystudio/beaver/data/local/SharedPreferencesHelper.kt
+++ app/src/main/kotlin/net/samystudio/beaver/data/local/SharedPreferencesHelper.kt
@@ -1,12 +1,22 @@
 package net.samystudio.beaver.data.local
 
+import android.content.Context
 import com.f2prateek.rx.preferences2.RxSharedPreferences
+import net.samystudio.beaver.R
+import net.samystudio.beaver.di.qualifier.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class SharedPreferencesHelper @Inject constructor(rxSharedPreferences: RxSharedPreferences) {
-    val apiUrl by lazy { rxSharedPreferences.getString(API_URL) }
+class SharedPreferencesHelper @Inject constructor(
+    @ApplicationContext context: Context, rxSharedPreferences: RxSharedPreferences
+) {
+    val apiUrl by lazy {
+        rxSharedPreferences.getString(
+            API_URL,
+            context.getString(R.string.api_url)
+        )
+    }
     val accountName by lazy { rxSharedPreferences.getString(ACCOUNT_NAME) }
     val accountToken by lazy { rxSharedPreferences.getString(ACCOUNT_TOKEN) }
 
