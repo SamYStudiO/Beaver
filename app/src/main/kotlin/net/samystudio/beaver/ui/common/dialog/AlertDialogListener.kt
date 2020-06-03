a6072ecaac021abe8394880e2a016ca9271e7a10
Samuel Eminet <contact@samystudio.net>
2020-06-03 10:53:44 -0400

    Doc

--- app/src/main/kotlin/net/samystudio/beaver/ui/common/dialog/AlertDialogListener.kt
+++ app/src/main/kotlin/net/samystudio/beaver/ui/common/dialog/AlertDialogListener.kt
@@ -1,5 +1,6 @@
 package net.samystudio.beaver.ui.common.dialog
 
+import android.content.DialogInterface
 import android.content.DialogInterface.OnClickListener
 import android.content.DialogInterface.OnMultiChoiceClickListener
 
@@ -9,33 +10,41 @@ import android.content.DialogInterface.OnMultiChoiceClickListener
  */
 interface AlertDialogListener : DialogListener {
     /**
-     * This is equivalent to [onDialogClick] (requestCode: Int, DialogInterface.BUTTON_POSITIVE).
+     * This is equivalent to [onDialogClick] ([requestCode]: [Int], [DialogInterface.BUTTON_POSITIVE]).
      * @see OnClickListener.onClick
      */
     fun onDialogPositive(requestCode: Int) {
     }
 
     /**
-     * This is equivalent to [onDialogClick] (requestCode: Int, DialogInterface.BUTTON_NEGATIVE).
+     * This is equivalent to [onDialogClick] ([requestCode]: [Int], [DialogInterface.BUTTON_NEGATIVE]).
      * @see OnClickListener.onClick
      */
     fun onDialogNegative(requestCode: Int) {
     }
 
     /**
-     * This is equivalent to [onDialogClick] (requestCode: Int, DialogInterface.BUTTON_NEUTRAL).
+     * This is equivalent to [onDialogClick] ([requestCode]: [Int], [DialogInterface.BUTTON_NEUTRAL]).
      * @see OnClickListener.onClick
      */
     fun onDialogNeutral(requestCode: Int) {
     }
 
     /**
+     * For a list returns index of clicked item.
+     * This won't return [DialogInterface.BUTTON_POSITIVE], [DialogInterface.BUTTON_NEGATIVE] or
+     * [DialogInterface.BUTTON_NEUTRAL], use [onDialogPositive], [onDialogNegative] or
+     * [onDialogNeutral] instead.
      * @see OnClickListener.onClick
      */
     fun onDialogClick(requestCode: Int, which: Int) {
     }
 
     /**
+     * For a list returns index checked/unchecked item.
+     * This won't return [DialogInterface.BUTTON_POSITIVE], [DialogInterface.BUTTON_NEGATIVE] or
+     * [DialogInterface.BUTTON_NEUTRAL], use [onDialogPositive], [onDialogNegative] or
+     * [onDialogNeutral] instead.
      * @see OnMultiChoiceClickListener.onClick
      */
     fun onDialogClick(requestCode: Int, which: Int, checked: Boolean) {
