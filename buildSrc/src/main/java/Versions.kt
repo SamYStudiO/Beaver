757b19b4413f5e582665142dc3bf1db2d4ed4146
SamYStudiO <contact@samystudio.net>
2020-09-30 11:12:34 -0400

    Bump

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -5,21 +5,21 @@ object Versions {
 
     // tools
     const val android_build_tools = "4.0.1"
-    const val google_services = "4.3.3"
-    const val firebase_crashlytics_plugin = "2.2.0"
+    const val google_services = "4.3.4"
+    const val firebase_crashlytics_plugin = "2.3.0"
     const val firebase_perf_plugin = "1.3.1"
     const val navigation_safe_args_plugin = "1.0.0"
-    const val hilt_android_plugin = "2.28.3-alpha"
-    const val gradle_versions_plugin = "0.29.0"
+    const val hilt_android_plugin = "2.29.1-alpha"
+    const val gradle_versions_plugin = "0.33.0"
 
     // kotlin
-    const val kotlin = "1.3.72"
+    const val kotlin = "1.4.10"
 
     // android
     const val core_ktx = "1.3.1"
-    const val appcompat = "1.1.0"
+    const val appcompat = "1.2.0"
     const val preference_ktx = "1.1.1"
-    const val constraintlayout = "1.1.3"
+    const val constraintlayout = "2.0.1"
     const val arch_version = "2.1.0"
     const val lifecycle = "2.2.0"
     const val navigation = "2.3.0"
@@ -27,33 +27,33 @@ object Versions {
     const val material = "1.3.0-alpha02"
 
     // firebase
-    const val firebase_core = "17.4.4"
+    const val firebase_core = "17.5.0"
     const val firebase_appindexing = "19.1.0"
-    const val firebase_crashlytics = "17.1.1"
-    const val firebase_analytics = "17.4.4"
-    const val firebase_perf = "19.0.8"
+    const val firebase_crashlytics = "17.2.2"
+    const val firebase_analytics = "17.5.0"
+    const val firebase_perf = "19.0.9"
 
     // dagger
-    const val dagger = "2.28.3"
-    const val assisted_inject = "0.5.2"
-    const val hilt_android = "2.28.3-alpha"
+    const val dagger = "2.29.1"
+    const val assisted_inject = "0.6.0"
+    const val hilt_android = "2.29.1-alpha"
     const val hilt_androidx = "1.0.0-alpha02"
 
     // reactive
-    const val rxjava = "3.0.5"
+    const val rxjava = "3.0.6"
     const val rxandroid = "3.0.0"
-    const val rxkotlin = "3.0.0"
+    const val rxkotlin = "3.0.1"
     const val rxbinding = "4.0.0"
-    const val rx_preferences = "2.0.0"
+    const val rx_preferences = "2.0.1"
     const val rxpicasso = "0.3.0"
 
     // network
-    const val okhttp = "4.8.0"
+    const val okhttp = "4.9.0"
     const val retrofit = "2.9.0"
     const val picasso = "3.0.0-SNAPSHOT"
 
     // misc
-    const val permissionsdispatcher = "4.7.0"
+    const val permissionsdispatcher = "4.8.0"
 
     // debug
     const val timber = "4.7.1"
@@ -61,9 +61,9 @@ object Versions {
 
     // test
     const val junit = "4.13"
-    const val test_core = "1.2.0"
-    const val robolectric = "4.3.1"
-    const val mockito = "3.4.6"
-    const val test_runner = "1.2.0"
-    const val espresso_core = "3.2.0"
+    const val test_core = "1.3.0"
+    const val robolectric = "4.4"
+    const val mockito = "3.5.13"
+    const val test_runner = "1.3.0"
+    const val espresso_core = "3.3.0"
 }
