09b32ab21a8dc05d606a660d1e295c39805e727e
Samuel Eminet <contact@samystudio.net>
2020-12-01 16:41:40 +0100

    Bump

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,39 +4,39 @@ object Versions {
     const val targetSdk = 30
 
     // tools
-    const val android_build_tools = "4.1.0"
+    const val android_build_tools = "4.1.1"
     const val google_services = "4.3.4"
-    const val firebase_crashlytics_plugin = "2.3.0"
-    const val firebase_perf_plugin = "1.3.2"
+    const val firebase_crashlytics_plugin = "2.4.1"
+    const val firebase_perf_plugin = "1.3.4"
     const val navigation_safe_args_plugin = "2.3.1"
-    const val hilt_android_plugin = "2.29.1-alpha"
-    const val gradle_versions_plugin = "0.33.0"
+    const val hilt_android_plugin = "2.30.1-alpha"
+    const val gradle_versions_plugin = "0.36.0"
 
     // kotlin
-    const val kotlin = "1.4.10"
+    const val kotlin = "1.4.20"
 
     // android
     const val core_ktx = "1.3.2"
     const val appcompat = "1.2.0"
     const val preference_ktx = "1.1.1"
-    const val constraintlayout = "2.0.2"
+    const val constraintlayout = "2.0.4"
     const val arch_version = "2.1.0"
     const val lifecycle = "2.2.0"
     const val navigation = "2.3.1"
     const val room = "2.2.5"
-    const val material = "1.3.0-alpha03"
+    const val material = "1.3.0-alpha04"
 
     // firebase
-    const val firebase_core = "17.5.1"
+    const val firebase_core = "18.0.0"
     const val firebase_appindexing = "19.1.0"
-    const val firebase_crashlytics = "17.2.2"
-    const val firebase_analytics = "17.6.0"
-    const val firebase_perf = "19.0.9"
+    const val firebase_crashlytics = "17.3.0"
+    const val firebase_analytics = "18.0.0"
+    const val firebase_perf = "19.0.10"
 
     // dagger
-    const val dagger = "2.29.1"
+    const val dagger = "2.30.1"
     const val assisted_inject = "0.6.0"
-    const val hilt_android = "2.29.1-alpha"
+    const val hilt_android = "2.30.1-alpha"
     const val hilt_androidx = "1.0.0-alpha02"
 
     // reactive
@@ -63,7 +63,7 @@ object Versions {
     const val junit = "4.13.1"
     const val test_core = "1.3.0"
     const val robolectric = "4.4"
-    const val mockito = "3.5.13"
+    const val mockito = "3.6.28"
     const val test_runner = "1.3.0"
     const val espresso_core = "3.3.0"
 }
