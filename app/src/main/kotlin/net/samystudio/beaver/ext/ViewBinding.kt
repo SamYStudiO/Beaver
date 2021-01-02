36ce6771e6c3d67a7c84197594b349f84fed4723
SamYStudiO <contact@samystudio.net>
2021-01-02 17:30:15 -0400

    Credit viewBinding delegates

--- app/src/main/kotlin/net/samystudio/beaver/ext/ViewBinding.kt
+++ app/src/main/kotlin/net/samystudio/beaver/ext/ViewBinding.kt
@@ -12,6 +12,10 @@ import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
+/**
+ * https://medium.com/@Zhuinden/simple-one-liner-viewbinding-in-fragments-and-activities-with-kotlin-961430c6c07c
+ */
+
 inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
     crossinline bindingInflater: (LayoutInflater) -> T
 ) = lazy(LazyThreadSafetyMode.NONE) {
