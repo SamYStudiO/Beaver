157bcf754a5db08ed8441f0c177f31332cb32798
SamYStudiO <contact@samystudio.net>
2021-01-03 21:36:00 -0400

    Add more device extensions + refactoring

--- app/src/main/kotlin/net/samystudio/beaver/ext/Device.kt
+++ app/src/main/kotlin/net/samystudio/beaver/ext/Device.kt
@@ -4,20 +4,40 @@ package net.samystudio.beaver.ext
 
 import android.annotation.SuppressLint
 import android.os.Build
+import net.samystudio.beaver.ContextProvider
 import java.util.*
 
-val deviceLanguage: String
+val deviceLocale: Locale
+    get() = Locale.getDefault()
+val deviceLocaleLanguage: String
     get() = Locale.getDefault().language
-val deviceLocale: String
+val deviceLocaleLanguageName: String
+    get() = Locale.getDefault().displayLanguage
+val deviceLocaleCountry: String
+    get() = Locale.getDefault().country
+val deviceLocaleCountryName: String
+    get() = Locale.getDefault().displayCountry
+val deviceLocaleName: String
     get() = Locale.getDefault().displayName
-val deviceTimeZone: String
+
+val deviceTimeZone: TimeZone
+    get() = TimeZone.getDefault()
+val deviceTimeZoneId: String
     get() = TimeZone.getDefault().id
-val deviceIsEmulator
-    get() = Build.FINGERPRINT.startsWith("generic")
-            || Build.FINGERPRINT.startsWith("unknown")
-            || Build.MODEL.contains("google_sdk")
-            || Build.MODEL.contains("Emulator")
-            || Build.MODEL.contains("Android SDK built for x86")
-            || Build.MANUFACTURER.contains("Genymotion")
-            || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
-            || Build.PRODUCT == "sdk_gphone_x86"
+val deviceTimeZoneName: String
+    get() = TimeZone.getDefault().displayName
+
+val deviceScreenDensity = ContextProvider.applicationContext.resources.displayMetrics.density
+val deviceScreenWidthPixels =
+    ContextProvider.applicationContext.resources.displayMetrics.widthPixels
+val deviceScreenHeightPixels =
+    ContextProvider.applicationContext.resources.displayMetrics.heightPixels
+
+val deviceIsEmulator = Build.FINGERPRINT.startsWith("generic")
+        || Build.FINGERPRINT.startsWith("unknown")
+        || Build.MODEL.contains("google_sdk")
+        || Build.MODEL.contains("Emulator")
+        || Build.MODEL.contains("Android SDK built for x86")
+        || Build.MANUFACTURER.contains("Genymotion")
+        || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+        || Build.PRODUCT == "sdk_gphone_x86"
