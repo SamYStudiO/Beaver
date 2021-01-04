028e2f7f182c551f480e715e41592e138a4dbe95
SamYStudiO <contact@samystudio.net>
2021-01-03 22:48:15 -0400

    Improve deviceIsEmulator

--- app/src/main/kotlin/net/samystudio/beaver/ext/Device.kt
+++ app/src/main/kotlin/net/samystudio/beaver/ext/Device.kt
@@ -33,11 +33,16 @@ val deviceScreenWidthPixels =
 val deviceScreenHeightPixels =
     ContextProvider.applicationContext.resources.displayMetrics.heightPixels
 
-val deviceIsEmulator = Build.FINGERPRINT.startsWith("generic")
+val deviceIsEmulator = (Build.FINGERPRINT.startsWith("google/sdk_gphone_")
+        && Build.FINGERPRINT.endsWith(":user/release-keys")
+        && Build.MANUFACTURER == "Google" && Build.PRODUCT.startsWith("sdk_gphone_") && Build.BRAND == "google"
+        && Build.MODEL.startsWith("sdk_gphone_"))
+        || Build.FINGERPRINT.startsWith("generic")
         || Build.FINGERPRINT.startsWith("unknown")
         || Build.MODEL.contains("google_sdk")
         || Build.MODEL.contains("Emulator")
         || Build.MODEL.contains("Android SDK built for x86")
         || Build.MANUFACTURER.contains("Genymotion")
-        || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
-        || Build.PRODUCT == "sdk_gphone_x86"
+        || Build.HOST.startsWith("Build")
+        || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
+        || Build.PRODUCT == "google_sdk"
