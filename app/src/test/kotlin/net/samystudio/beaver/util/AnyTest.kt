3d6cae9cce63c2dfe80b266a3398c4da540bd692
SamYStudiO <contact@samystudio.net>
2021-03-20 19:03:49 -0400

    Fix tests

--- app/src/test/kotlin/net/samystudio/beaver/util/AnyTest.kt
+++ app/src/test/kotlin/net/samystudio/beaver/util/AnyTest.kt
@@ -7,6 +7,6 @@ class AnyTest {
 
     @Test
     fun testGetMethodTag() {
-        assertEquals("net.samystudio.beaver.ext.AnyTest::testGetMethodTag", getMethodTag())
+        assertEquals("net.samystudio.beaver.util.AnyTest::testGetMethodTag", getMethodTag())
     }
 }
