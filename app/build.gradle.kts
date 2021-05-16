1e6b7e8570fc9f67a869f23a68bde1b2db88a383
SamYStudiO <contact@samystudio.net>
2021-05-16 09:46:50 -0400

    Bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,15 +87,6 @@ android {
         viewBinding = true
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-
     hilt {
         enableTransformForLocalTests = true
     }
