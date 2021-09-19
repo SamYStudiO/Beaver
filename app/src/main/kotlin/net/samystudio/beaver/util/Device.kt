ec2dd54b08148a052c5b34f6b9c0f5f5430a863d
SamYStudiO <contact@samystudio.net>
2021-09-19 11:52:37 -0400

    Cleanup device utils

--- app/src/main/kotlin/net/samystudio/beaver/util/Device.kt
+++ app/src/main/kotlin/net/samystudio/beaver/util/Device.kt
@@ -1,40 +1,6 @@
-@file:SuppressLint("HardwareIds")
-@file:Suppress("unused")
-
 package net.samystudio.beaver.util
 
-import android.annotation.SuppressLint
 import android.os.Build
-import net.samystudio.beaver.ContextProvider.Companion.applicationContext
-import java.util.*
-
-const val device = "android"
-val deviceModel = Build.BRAND + "|" + Build.MODEL + "|" + Build.VERSION.RELEASE
-val deviceLocale: Locale
-    get() = Locale.getDefault()
-val deviceLocaleLanguage: String
-    get() = Locale.getDefault().language
-val deviceLocaleLanguageName: String
-    get() = Locale.getDefault().displayLanguage
-val deviceLocaleCountry: String
-    get() = Locale.getDefault().country
-val deviceLocaleCountryName: String
-    get() = Locale.getDefault().displayCountry
-val deviceLocaleName: String
-    get() = Locale.getDefault().displayName
-
-val deviceTimeZone: TimeZone
-    get() = TimeZone.getDefault()
-val deviceTimeZoneId: String
-    get() = TimeZone.getDefault().id
-val deviceTimeZoneName: String
-    get() = TimeZone.getDefault().displayName
-
-val deviceScreenDensity = applicationContext.resources.displayMetrics.density
-val deviceScreenWidthPixels =
-    applicationContext.resources.displayMetrics.widthPixels
-val deviceScreenHeightPixels =
-    applicationContext.resources.displayMetrics.heightPixels
 
 val deviceIsEmulator = (
         Build.FINGERPRINT.startsWith("google/sdk_gphone_") &&
